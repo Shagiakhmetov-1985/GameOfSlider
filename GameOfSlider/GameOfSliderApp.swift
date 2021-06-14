60d6da89935cd56d822df7c520953eded8d744c7
Marat <Shagiakhmetov2499@gmail.com>
2021-06-14 15:40:18 +0300

    Initial Commit

---
+++ GameOfSlider/GameOfSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfSliderApp.swift
+//  GameOfSlider
+//
+//  Created by Marat Shagiakhmetov on 14.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct GameOfSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
